a97b8903f69c406aa75f6057089a5cb372f2647c
Joshua230670 <Joshua230670@gmail.com>
2026-01-15 15:52:04 -0500

    Finish TODOs

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ print(f"There are {len(products)} products in the 'YoungFolks' store.")
 for p in products:
     print("Title:", p["title"])
     print("Category:", p["category"])
+    print("Rate:", p["rating"]["rate"])
+    print("Price:", p["price"])
+    print("Image:", p["image"])
     print()
 
-#TODO: include the rate (not the count) of the product
-#TODO: include the price
-#TODO: include the picture of the product
+#TODO: include the rate (not the count) of the product (Done)
+#TODO: include the price (Done)
+#TODO: include the picture of the product (Done)
